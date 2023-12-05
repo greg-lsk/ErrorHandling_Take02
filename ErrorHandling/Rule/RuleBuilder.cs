d2122eb44eb654ea4e6bd95915942557f2d2e1c2
greg-lsk <greg.liaskas@outlook.com>
2023-12-05 08:57:11 +0200

    Introduced RuleBuilder for Streamlined DomainRule Creation
    This commit adds the RuleBuilder static class, a user-friendly interface for creating DomainRule instances. This addition simplifies the rule creation process and enhances the overall user experience.
    
    Analysis of Changes:
    - **Streamlined Rule Creation**: RuleBuilder offers static methods that allow users to create DomainRule instances using either a `Func<TSubject, bool>` or a `RuleSequence`. This simplifies the rule creation process and makes it more intuitive.
    - **Support for Various Rule Types**: The RuleBuilder supports the creation of both DirectRule and IndirectRule instances, accommodating a wide range of scenarios.

--- ErrorHandling/Rule/RuleBuilder.cs
+++ ErrorHandling/Rule/RuleBuilder.cs
@@ -2,8 +2,28 @@
 
 public static class RuleBuilder
 {
-    public static DomainRule<TSubject> Create<TSubject>(Func<TSubject, bool> predicate,
-                                                        Enum incomplianceTag,
-                                                        IncomplianceSeverity incomplianceSeverity)
-        => new(predicate, incomplianceTag, incomplianceSeverity);
+    public static DomainRule Create<TSubject>(Func<TSubject, bool> predicate,
+                                              Enum incomplianceTag,
+                                              IncomplianceSeverity severity)
+        => new DirectRule<TSubject>(predicate, incomplianceTag, severity);
+
+
+    public static DomainRule Create<TSubject>(RuleSequence<TSubject> sequence,
+                                              Enum incomplianceTag,
+                                              IncomplianceSeverity severity)
+        => new DirectRule<TSubject>(sequence, incomplianceTag, severity);
+
+
+    public static DomainRule Create<TSubject, TProperty>(Func<TSubject, TProperty> selector,
+                                                         Func<TSubject, bool> predicate,
+                                                         Enum incomplianceTag,
+                                                         IncomplianceSeverity severity)
+        => new IndirectRule<TSubject, TProperty>(predicate, incomplianceTag, severity, selector);
+
+
+    public static DomainRule Create<TSubject, TProperty>(Func<TSubject, TProperty> selector,
+                                                         RuleSequence<TProperty> sequence,
+                                                         Enum incomplianceTag,
+                                                         IncomplianceSeverity severity)
+    => new IndirectRule<TSubject, TProperty>(sequence, incomplianceTag, severity, selector);
 }
