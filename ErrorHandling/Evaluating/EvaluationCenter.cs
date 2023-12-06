466b9bc47d0c6e450623aadc55fefef248e4e0de
greg-lsk <greg.liaskas@outlook.com>
2023-12-06 06:13:49 +0200

    Draft for EvaluationCenter Class Created
    This class will be used to assign the correct evaluation method based on the type of the `DomainRule`.
    
    This design allows us to handle the `TProperty` type being unknown when calling an external function. It also helps us infer the type and avoid the use of reflection.

---
+++ ErrorHandling/Evaluating/EvaluationCenter.cs
@@ -0,0 +1,25 @@
+﻿using ErrorHandling.Rule;
+
+
+namespace ErrorHandling.Evaluating;
+
+internal static class EvaluationCenter
+{
+    internal static bool Evaluate<TSubject, TProperty>(in Evaluation evaluation,
+                                                       TSubject subject,
+                                                       DomainRule rule)
+    {
+        var indirectRule = rule as IndirectRule<TSubject, TProperty>;
+
+        return true;
+    }
+
+    internal static bool Evaluate<TSubject>(in Evaluation evaluation,
+                                            TSubject subject,
+                                            DomainRule rule)
+    {
+        var directRule = rule as DirectRule<TSubject>;
+
+        return true;
+    }
+}
