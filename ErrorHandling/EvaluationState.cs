c30dfcfffcc2805e1c0d2451b0a289d32c6237ef
greg-lsk <greg.liaskas@outlook.com>
2023-12-14 00:44:44 +0200

    Refactor Result creation logic
    This commit removes all the logic related to creating Results from the class, since it is now delegated to the ResultPipe. This way, the class is more cohesive and follows the single responsibility principle.

--- ErrorHandling/EvaluationState.cs
+++ ErrorHandling/EvaluationState.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 using ErrorHandling.Reporting;
 using ErrorHandling.Reporting.Logging;
 using ErrorHandling.Reporting.CallStackInfo;
-using ErrorHandling.ResultUtilities;
 
 
 namespace ErrorHandling;
@@ -31,42 +30,5 @@ public readonly partial struct EvaluationState
     {
         var logger = EvaluationLogger.Get<TCategory>();
         return new(logger, callerMemberName, callerLineNumber);
-    }
-
-    public Result<TResult> YieldResult<TResult>(Func<TResult> yieldDelegate)
-    {
-        if (Report.HasErrors) return Result<TResult>();
-        
-        return new(yieldDelegate.Invoke());
-    }
-
-    public VoidResult YieldVoid(Action yieldAction)
-    {
-        if (Report.HasErrors) return Void();
-
-        yieldAction.Invoke();
-        return new();
-    }
-
-
-    private VoidResult Void()
-    {
-        Logger.Log(
-            LogLevel.Error,
-            "{TraceInfo}" +
-            "      {ReportString}" +
-            "\n      [EvaluationID]:{ReportID}", TraceInfo, Report.ToString(), Report.ReportId);
-
-        return new(new ResultReport(Report.ReportId, Report.Flags!));
-    }
-    private Result<TReturn> Result<TReturn>()
-    {
-        Logger.Log(
-            LogLevel.Error,
-            "{TraceInfo}" +
-            "      {ReportString}" +
-            "\n      [EvaluationID]:{ReportID}", TraceInfo, Report.ToString(), Report.ReportId);
-
-        return new(new ResultReport(Report.ReportId, Report.Flags!));
     }
 }
